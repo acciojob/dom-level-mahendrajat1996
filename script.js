f32fab4a1678ecebab90d990d48a492b15f2d5c0
mahendrajat1996 <130475100+mahendrajat1996@users.noreply.github.com>
2023-07-03 22:22:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function removeColor() {
+  // Get the dropdown list element
+  const dropdown = document.getElementById("colorSelect");
+
+  // Get the selected color option
+  const selectedColor = dropdown.value;
+
+  // Find the index of the selected color option
+  const selectedIndex = dropdown.selectedIndex;
+
+  // Remove the selected color option from the dropdown list
+  dropdown.remove(selectedIndex);
+
+  // Display the final answer using alert()
+  alert(`The level of the element is: ${selectedIndex + 1}`);
+}
